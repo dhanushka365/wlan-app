c6f68d5e32b0596666ec80d9bd8063e50ec7d9c5
dhanushka365 <technohub10000@gmail.com>
2023-06-09 19:31:06 +0530

    ADMIN LTE popup removed

--- resources/views/admin/layout/layout.blade.php
+++ resources/views/admin/layout/layout.blade.php
@@ -60,8 +60,7 @@
     <!-- ChartJS -->
     <script src="{{ url ('admin/plugins/chart.js/Chart.min.js')}}"></script>
 
-    <!-- AdminLTE for demo purposes -->
-    <script src="{{ url ('admin/js/demo.js')}}"></script>
+
     <!-- AdminLTE dashboard demo (This is only for demo purposes) -->
     <script src="{{ url ('admin/js/pages/dashboard2.js')}}"></script>
 </body>
