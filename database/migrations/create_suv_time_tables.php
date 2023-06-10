d13232eee6dfd32ce9a1e05341073fc3a9707dd8
DJCharuka98 <jayamalicharuka@gmail.com>
2023-06-10 08:10:40 +0530

    Surv database table migration

---
+++ database/migrations/create_suv_time_tables.php
@@ -0,0 +1,36 @@
+
+
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('time_table', function (Blueprint $table) {
+            $table->id();
+            $table->string('event_name', 255);
+            $table->dateTime('event_time');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('time_table');
+    }
+};
+
