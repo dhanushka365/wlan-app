28a454a44b94d6038c049bf6dc683271eaebda15
dhanushka365 <technohub10000@gmail.com>
2023-06-10 11:00:16 +0530

    unwanted codes commeted

--- resources/views/admin/layout/sidebar.blade.php
+++ resources/views/admin/layout/sidebar.blade.php
@@ -68,7 +68,7 @@
                          </li>
                      </ul>
                  </li>
-                 <li class="nav-item">
+                 <!-- <li class="nav-item">
                      <a href="pages/widgets.html" class="nav-link">
                          <i class="nav-icon fas fa-th"></i>
                          <p>
@@ -76,8 +76,8 @@
                              <span class="right badge badge-danger">New</span>
                          </p>
                      </a>
-                 </li>
-                 <li class="nav-item">
+                 </li> -->
+                 <!-- <li class="nav-item">
                      <a href="#" class="nav-link">
                          <i class="nav-icon fas fa-copy"></i>
                          <p>
@@ -136,8 +136,8 @@
                              </a>
                          </li>
                      </ul>
-                 </li>
-                 <li class="nav-item">
+                 </li> -->
+                 <!-- <li class="nav-item">
                      <a href="#" class="nav-link">
                          <i class="nav-icon fas fa-chart-pie"></i>
                          <p>
@@ -265,8 +265,8 @@
                              </a>
                          </li>
                      </ul>
-                 </li>
-                 <li class="nav-item">
+                 </li> -->
+                 <!-- <li class="nav-item">
                      <a href="#" class="nav-link">
                          <i class="nav-icon fas fa-table"></i>
                          <p>
@@ -349,8 +349,8 @@
                              </a>
                          </li>
                      </ul>
-                 </li>
-                 <li class="nav-item">
+                 </li> -->
+                 <!-- <li class="nav-item">
                      <a href="#" class="nav-link">
                          <i class="nav-icon fas fa-book"></i>
                          <p>
@@ -420,8 +420,8 @@
                              </a>
                          </li>
                      </ul>
-                 </li>
-                 <li class="nav-item">
+                 </li> -->
+                 <!-- <li class="nav-item">
                      <a href="#" class="nav-link">
                          <i class="nav-icon far fa-plus-square"></i>
                          <p>
@@ -464,93 +464,93 @@
                                      </a>
                                  </li>
                              </ul>
-                         </li>
+                         </li> -->
+                 <!-- <li class="nav-item">
+                     <a href="#" class="nav-link">
+                         <i class="far fa-circle nav-icon"></i>
+                         <p>
+                             Login & Register v2
+                             <i class="fas fa-angle-left right"></i>
+                         </p>
+                     </a>
+                     <ul class="nav nav-treeview">
                          <li class="nav-item">
-                             <a href="#" class="nav-link">
+                             <a href="pages/examples/login-v2.html" class="nav-link">
                                  <i class="far fa-circle nav-icon"></i>
-                                 <p>
-                                     Login & Register v2
-                                     <i class="fas fa-angle-left right"></i>
-                                 </p>
-                             </a>
-                             <ul class="nav nav-treeview">
-                                 <li class="nav-item">
-                                     <a href="pages/examples/login-v2.html" class="nav-link">
-                                         <i class="far fa-circle nav-icon"></i>
-                                         <p>Login v2</p>
-                                     </a>
-                                 </li>
-                                 <li class="nav-item">
-                                     <a href="pages/examples/register-v2.html" class="nav-link">
-                                         <i class="far fa-circle nav-icon"></i>
-                                         <p>Register v2</p>
-                                     </a>
-                                 </li>
-                                 <li class="nav-item">
-                                     <a href="pages/examples/forgot-password-v2.html" class="nav-link">
-                                         <i class="far fa-circle nav-icon"></i>
-                                         <p>Forgot Password v2</p>
-                                     </a>
-                                 </li>
-                                 <li class="nav-item">
-                                     <a href="pages/examples/recover-password-v2.html" class="nav-link">
-                                         <i class="far fa-circle nav-icon"></i>
-                                         <p>Recover Password v2</p>
-                                     </a>
-                                 </li>
-                             </ul>
-                         </li>
-                         <li class="nav-item">
-                             <a href="pages/examples/lockscreen.html" class="nav-link">
-                                 <i class="far fa-circle nav-icon"></i>
-                                 <p>Lockscreen</p>
+                                 <p>Login v2</p>
                              </a>
                          </li>
                          <li class="nav-item">
-                             <a href="pages/examples/legacy-user-menu.html" class="nav-link">
+                             <a href="pages/examples/register-v2.html" class="nav-link">
                                  <i class="far fa-circle nav-icon"></i>
-                                 <p>Legacy User Menu</p>
+                                 <p>Register v2</p>
                              </a>
                          </li>
                          <li class="nav-item">
-                             <a href="pages/examples/language-menu.html" class="nav-link">
+                             <a href="pages/examples/forgot-password-v2.html" class="nav-link">
                                  <i class="far fa-circle nav-icon"></i>
-                                 <p>Language Menu</p>
+                                 <p>Forgot Password v2</p>
                              </a>
                          </li>
                          <li class="nav-item">
-                             <a href="pages/examples/404.html" class="nav-link">
+                             <a href="pages/examples/recover-password-v2.html" class="nav-link">
                                  <i class="far fa-circle nav-icon"></i>
-                                 <p>Error 404</p>
-                             </a>
-                         </li>
-                         <li class="nav-item">
-                             <a href="pages/examples/500.html" class="nav-link">
-                                 <i class="far fa-circle nav-icon"></i>
-                                 <p>Error 500</p>
-                             </a>
-                         </li>
-                         <li class="nav-item">
-                             <a href="pages/examples/pace.html" class="nav-link">
-                                 <i class="far fa-circle nav-icon"></i>
-                                 <p>Pace</p>
-                             </a>
-                         </li>
-                         <li class="nav-item">
-                             <a href="pages/examples/blank.html" class="nav-link">
-                                 <i class="far fa-circle nav-icon"></i>
-                                 <p>Blank Page</p>
-                             </a>
-                         </li>
-                         <li class="nav-item">
-                             <a href="starter.html" class="nav-link">
-                                 <i class="far fa-circle nav-icon"></i>
-                                 <p>Starter Page</p>
+                                 <p>Recover Password v2</p>
                              </a>
                          </li>
                      </ul>
+                 </li> -->
+                 <!-- <li class="nav-item">
+                     <a href="pages/examples/lockscreen.html" class="nav-link">
+                         <i class="far fa-circle nav-icon"></i>
+                         <p>Lockscreen</p>
+                     </a>
                  </li>
                  <li class="nav-item">
+                     <a href="pages/examples/legacy-user-menu.html" class="nav-link">
+                         <i class="far fa-circle nav-icon"></i>
+                         <p>Legacy User Menu</p>
+                     </a>
+                 </li>
+                 <li class="nav-item">
+                     <a href="pages/examples/language-menu.html" class="nav-link">
+                         <i class="far fa-circle nav-icon"></i>
+                         <p>Language Menu</p>
+                     </a>
+                 </li>
+                 <li class="nav-item">
+                     <a href="pages/examples/404.html" class="nav-link">
+                         <i class="far fa-circle nav-icon"></i>
+                         <p>Error 404</p>
+                     </a>
+                 </li>
+                 <li class="nav-item">
+                     <a href="pages/examples/500.html" class="nav-link">
+                         <i class="far fa-circle nav-icon"></i>
+                         <p>Error 500</p>
+                     </a>
+                 </li>
+                 <li class="nav-item">
+                     <a href="pages/examples/pace.html" class="nav-link">
+                         <i class="far fa-circle nav-icon"></i>
+                         <p>Pace</p>
+                     </a>
+                 </li>
+                 <li class="nav-item">
+                     <a href="pages/examples/blank.html" class="nav-link">
+                         <i class="far fa-circle nav-icon"></i>
+                         <p>Blank Page</p>
+                     </a>
+                 </li>
+                 <li class="nav-item">
+                     <a href="starter.html" class="nav-link">
+                         <i class="far fa-circle nav-icon"></i>
+                         <p>Starter Page</p>
+                     </a>
+                 </li>
+             </ul>
+             </li> -->
+                 <!-- <li class="nav-item">
                      <a href="#" class="nav-link">
                          <i class="nav-icon fas fa-search"></i>
                          <p>
@@ -671,8 +671,8 @@
                      </a>
                  </li>
              </ul>
-         </nav>
-         <!-- /.sidebar-menu -->
+         </nav> -->
+                 <!-- /.sidebar-menu -->
      </div>
      <!-- /.sidebar -->
  </aside>
