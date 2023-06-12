4be5de576d374b9f755347c80049874c3c58fe0f
unknown <kesaramadushan.km@gmail.com>
2023-06-12 09:48:07 +0530

    Surveillance video feed alt

--- resources/views/admin/Surveillance_dashboard.blade.php
+++ resources/views/admin/Surveillance_dashboard.blade.php
@@ -26,25 +26,25 @@
             <!-- <div class="embed-responsive embed-responsive-16by9">
                 <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/tgbNymZ7vqY"></iframe>
             </div> -->
-            <img src="http://127.0.0.1:5000/video_feed_1" class="center-block" width="80%"/>
+            <img src="http://127.0.0.1:5000/video_feed_1" class="center-block" width="80%" alt="Offline"/>
         </div>
         <div class="col-sm-3">
             <!-- <div class="embed-responsive embed-responsive-16by9">
                 <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/tgbNymZ7vqY"></iframe>
             </div> -->
-            <img src="http://127.0.0.1:5000/video_feed_1" width="80%"/>
+            <img src="http://127.0.0.1:5000/video_feed_2" width="80%" alt="Offline"/>
         </div>
         <div class="col-sm-3">
             <!-- <div class="embed-responsive embed-responsive-16by9">
                 <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/tgbNymZ7vqY"></iframe>
             </div> -->
-            <img src="http://127.0.0.1:5000/video_feed_1" width="80%"/>
+            <img src="http://127.0.0.1:5000/video_feed_3" width="80%" alt="Offline"/>
         </div>
         <div class="col-sm-3">
             <!-- <div class="embed-responsive embed-responsive-16by9">
                 <iframe class="embed-responsive-item" src="http://127.0.0.1:5000/video_feed_1"></iframe>
             </div> -->
-            <img src="http://127.0.0.1:5000/video_feed_1" width="80%"/>
+            <img src="http://127.0.0.1:5000/video_feed_4" width="80%" alt="Offline"/>
         </div>
     </div>
 
