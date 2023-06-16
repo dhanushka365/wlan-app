9ac3c5cc10f3e3ba5442869b153846136ac840a2
dhanushka365 <technohub10000@gmail.com>
2023-06-16 22:22:31 +0530

    App name modified

--- resources/views/admin/layout/sidebar.blade.php
+++ resources/views/admin/layout/sidebar.blade.php
@@ -2,9 +2,12 @@
  <aside class="main-sidebar sidebar-dark-primary elevation-4">
      <!-- Brand Logo -->
      <a href="index3.html" class="brand-link">
-         <img src="{{ asset('admin/images/AdminLTELogo.png')}}" alt="AdminLTE Logo"
-             class="brand-image img-circle elevation-3" style="opacity: .8">
-         <span class="brand-text font-weight-light">WLAN-APP</span>
+         <img src="{{ asset('admin/images/logo1.png')}}" alt="AdminLTE Logo" class="brand-image img-circle elevation-3"
+             style="opacity: .8">
+         <span class="brand-text font-weight-bold">
+             <span style="color: red; font-style: italic;">W</span>LAN-
+             <span style="color: red; font-style: italic;">A</span>PP
+         </span>
      </a>
 
      <!-- Sidebar -->
